d8637986279f4711a051e3a6608a09f14b011e60
sonipritam <pritam.kumar@venturepact.com>
2019-06-05 08:05:57 +0530

    login page done

---
+++ components/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Inventery</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i,800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./../assets/css/main.css" />
+
+</head>
+
+<body>
+    <div class="forms-outeress">
+        <div class="well">
+            <h2>Sign In</h2>
+            <form class="material-form">
+                <div class="form-group">
+                    <input type="email" class="form-control">
+                    <label>Email</label>
+                    <span class="input-border"></span>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control">
+                    <label>Password</label>
+                    <span class="input-border"></span>
+                </div>
+                <button type="submit" class="btns btn-primary btn-lg">Submit</button>
+                <!-- <p class="helper-text">Don't have an account? <a href="#">Sign up</a> here.</p> -->
+            </form>
+        </div>
+    </div>
+
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="./../assets/js/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</html>
